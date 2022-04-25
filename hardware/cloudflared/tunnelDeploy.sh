ba344822a9f62310632a41ee8548f47c046a2555
Harshit Mani Tripathi <vandannaamhaimera@gmail.com>
2022-04-26 03:07:26 +0530

    added config example credit:IBRACORP

--- hardware/cloudflared/tunnelDeploy.sh
+++ hardware/cloudflared/tunnelDeploy.sh
@@ -6,9 +6,30 @@ if (( $EUID != 0 )); then
   exit 1;
 fi
 
-if [ $(which cloudflared) ]; then
-  echo "Cloudflare is already installed"
-  exit
-else
-  cfTunnelInstall
-fi
+printf "WARNING: Must have already logged in your cloudflare account beofre running script\n"
+printf "If logged in, Enter Y/y to continue else any other key to exit\n"
+read contLogin
+case $contLogin in
+  Y|y)
+    if [ $(which cloudflared) ]; then
+      cloudflared tunnel login
+
+      printf "Enter name for your tunnel: "
+      read tunName
+      cloudflared tunnel create $tunName
+
+      read -s -n 1 -p "Copy tunnel UUID and press enter to open config file"
+      # printf "Enter name for your tunnel: "
+      # read tunName
+      # echo $tunName
+    else
+      printf "Cloudflared is not installed. Try: https://github.com/Hudater/services/blob/main/hardware/cloudflared/debInstall.sh"
+    fi
+      ;;
+
+  *)
+    printf "Exiting script with code 1"
+    unset contLogin
+    exit 1;
+    ;;
+esac
