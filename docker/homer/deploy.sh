03ed17266a51928fba7b2d43b1c537f3e1e754a3
Harshit Mani Tripathi <vandannaamhaimera@gmail.com>
2022-04-26 15:08:07 +0530

    homer script fixed

--- docker/homer/deploy.sh
+++ docker/homer/deploy.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
 # Creating directories to hold config files etc
-mkdir -p "${BAK_CFG_DIR}"/homer
+mkdir -p /home/$USER/.config/homer
+touch "${BAK_CFG_DIR}"/homer/config.yml
 mkdir -p "${BAK_CFG_DIR}"/homer/manicons
 
 # deploying container
 docker-compose up -d
 
 # cloning custom theme for homer
-git clone https://github.com/WalkxCode/Homer-Theme.git /home/$USER/Bench/Homer-Theme
+git clone https://github.com/WalkxCode/Homer-Theme.git /home/$USER/Homer-Theme
+sudo chmod -R 777 /home/$USER/Homer-Theme
 
 # deleting default config. DON'T DO THIS IF YOU DON'T WANT CUSTOM THEME
-find "${BAK_CFG_DIR}"/homer/* ! -name 'manicons' -type d,f -exec rm -rf "{}" +
+sudo chmod -R 777 /home/$USER/.config/homer
+find /home/$USER/.config/homer/* ! -name 'manicons' ! -name 'config.yml' -type d,f -exec rm -rf "{}" +
 
 # moving custom theme files to homer dir
-mv /home/$USER/Bench/Homer-Theme/assets/* "${BAK_CFG_DIR}"/homer/
+mv /home/$USER/Homer-Theme/assets/config.yml /home/$USER/Homer-Theme/assets/og-config.yml
+mv /home/$USER/Homer-Theme/assets/* /home/$USER/.config/homer/
 
 # removing the custom theme git dir
-rm -rf /home/$USER/Bench/Homer-Theme
+rm -rf /home/$USER/Homer-Theme
