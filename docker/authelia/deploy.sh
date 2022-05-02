7a6b55d9b4ffc73ab00a4ed0d59051d128f19266
Harshit Mani Tripathi <vandannaamhaimera@gmail.com>
2022-05-02 21:56:27 +0530

    authelia to proper db

--- docker/authelia/deploy.sh
+++ docker/authelia/deploy.sh
@@ -1,4 +1,5 @@
 mkdir -p "${BAK_CFG_DIR}"/authelia
 sudo chmod -R 777 "${BAK_CFG_DIR}"/authelia/
-mkdir -p "${CFG_DIR}"/authelia
+mkdir -p "${DB_CFG_DIR}"/redis_authelia
+mkdir -p "${DB_CFG_DIR}"/mariadb_authelia
 docker-compose up -d
