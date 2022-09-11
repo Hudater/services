8ce5770ad1bf2be0bd42f2284f3ce16f5ab9acd2
Harshit Mani Tripathi <hmanitops@gmail.com>
2022-09-11 18:48:48 +0530

    two manifests for unmanic; TODO fix names to standard for machine-service

---
+++ docker/unmanic-gpu/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p ${CFG_DIR}/unmanic-gpu
+docker compose up -d
--- docker/unmanic/deploy.sh
+++ docker/unmanic/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 mkdir -p ${CFG_DIR}/unmanic
-mkdir -p /storage/unmanic/cache
-#mkdir -p /storage/unmanic/Library
 docker compose up -d
