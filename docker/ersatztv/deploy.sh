c5b4c1d2eb3b170f42df6ccdb2374a7b3880afcf
Harshit Mani Tripathi <hmanitops@gmail.com>
2022-10-12 17:55:33 +0530

    back to adguard now as opnsense

---
+++ docker/ersatztv/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p ${CFG_DIR}/ersatztv
+docker compose up -d
---
+++ docker/posterr/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir -p ${CFG_DIR}/posterr/config
+mkdir -p ${CFG_DIR}/posterr/custom
+docker compose up -d
