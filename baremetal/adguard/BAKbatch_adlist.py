fba5f5b01b30bb6cc150307c119178cb3c62f528
Harshit Mani Tripathi <hmanitops@gmail.com>
2023-05-28 13:01:18 +0530

    adguard batch add script replaced

--- baremetal/adguard/BAKbatch_adlist.py
+++ baremetal/adguard/BAKbatch_adlist.py
@@ -1,36 +1,28 @@
+## credit: https://www.reddit.com/r/Adguard/comments/isfjz3/adguard_home_batch_block_list_made_easy/
 from requests.adapters import HTTPAdapter
-
 from requests.packages.urllib3.poolmanager import PoolManager
-
 import ssl
-
 import requests
-
 import json
+
+## DEPENDENCY:
+# pip install requests
 
 ## CHANGE HERE ##
 # ip address of AdGuard Home
 # "http(s)://<adguardHomeIp:<port>"
 host = "http://10.0.0.1:8080"
-
 # user name
-userName = "ADGUARD_USERNAME"
+userName = "USERNAME_HERE"
 # password
-password = "ADGUARD_PASSWORD"
+password = "PASSWORD_HERE"
 
-# block list 
+# block list
 # taken from Wally3K's Firebog https://firebog.net/
 urls = [
 "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt",
 "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts",
 "https://v.firebog.net/hosts/static/w3kbl.txt",
-"https://raw.githubusercontent.com/matomo-org/referrer-spam-blacklist/master/spammers.txt",
-"https://someonewhocares.org/hosts/zero/hosts",
-"https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts",
-"https://winhelp2002.mvps.org/hosts.txt",
-"https://v.firebog.net/hosts/neohostsbasic.txt",
-"https://raw.githubusercontent.com/RooneyMcNibNug/pihole-stuff/master/SNAFU.txt",
-"https://paulgb.github.io/BarbBlock/blacklists/hosts-file.txt",
 "https://adaway.org/hosts.txt",
 "https://v.firebog.net/hosts/AdguardDNS.txt",
 "https://v.firebog.net/hosts/Admiral.txt",
@@ -40,17 +32,11 @@ urls = [
 "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
 "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts",
 "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts",
-"https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts",
 "https://v.firebog.net/hosts/Easyprivacy.txt",
 "https://v.firebog.net/hosts/Prigent-Ads.txt",
 "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts",
 "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt",
 "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt",
-"https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt",
-"https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt",
-"https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt",
-"https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/AmazonFireTV.txt",
-"https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt",
 "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt",
 "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt",
 "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt",
@@ -58,18 +44,17 @@ urls = [
 "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts",
 "https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt",
 "https://phishing.army/download/phishing_army_blocklist_extended.txt",
+"https://malware-filter.gitlab.io/malware-filter/phishing-filter-hosts.txt",
 "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt",
 "https://v.firebog.net/hosts/RPiList-Malware.txt",
 "https://v.firebog.net/hosts/RPiList-Phishing.txt",
 "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt",
 "https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts",
 "https://urlhaus.abuse.ch/downloads/hostfile/",
-"https://malware-filter.gitlab.io/malware-filter/phishing-filter-hosts.txt",
-"https://v.firebog.net/hosts/Prigent-Malware.txt",
 "https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser",
-"https://raw.githubusercontent.com/chadmayfield/my-pihole-blocklists/master/lists/pi_blocklist_porn_top1m.list",
-"https://v.firebog.net/hosts/Prigent-Adult.txt",
-"https://raw.githubusercontent.com/anudeepND/blacklist/master/facebook.txt",
+]
+allow_urls = [
+"https://raw.githubusercontent.com/Hudater/services/main/baremetal/adguard/allowlist.txt",
 ]
 
 ############ End Edits #################
@@ -81,8 +66,8 @@ class MyAdapter(HTTPAdapter):
                                        maxsize=maxsize,
                                        block=block,
                                        ssl_version=ssl.PROTOCOL_TLSv1)
- 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0', 'Content-type': 'application/json'}
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0'}     
 
 s = requests.Session()
 s.mount(host, MyAdapter())
@@ -90,7 +75,11 @@ x = s.post(host + "/control/login", json.dumps({"name": userName, "password" : p
 print(x.text)
 
 for u in urls:
-    filterObj = json.dumps({'url':u, "name":u,"whitelist":False})
-    print(filterObj)
-    x = s.post(host + "/control/filtering/add_url", data = filterObj, headers=headers)
-    print(x.text)
+	filterObj = json.dumps({'url':u, "name":u,"whitelist":False})
+	print(filterObj)
+	x = s.post(host + "/control/filtering/add_url", data = filterObj, headers=headers)
+	print(x.text)
+
+# help from 
+# https://stackoverflow.com/questions/30946370/using-requests-to-login-to-a-website-that-has-javascript-login-form
+# https://stackoverflow.com/questions/33818206/python-login-into-a-website-with-javascript-form
