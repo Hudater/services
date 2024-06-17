ee2354904053125cd0c3a481a78303eca6626792
Harshit Mani Tripathi <hmanitops@gmail.com>
2024-06-17 17:56:09 +0530

    gluetun, flaresolverr, prowlarr consolidated; wallos for subscription tracking; linkwarden TBD

--- docker/unmanic/deploy.sh
+++ docker/unmanic/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mkdir -p ${CFG_DIR}/unmanic
+#mkdir -p /mnt/storage/Unmanic
+mkdir -p /var/tmp/unmanic
 docker compose up -d
---
+++ docker/wallos/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir -p ${CFG_DIR}/wallos/db
+mkdir -p ${CFG_DIR}/wallos/logos
+docker compose up -d
