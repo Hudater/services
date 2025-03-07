e8595a9230e462d4572c77d93f8c5d7a4ffb5901
Harshit Mani Tripathi <hmanitops@gmail.com>
2025-03-08 01:04:47 +0530

    added whoami

---
+++ docker/whoami/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker compose up -d
+
+
+### Credit: https://askubuntu.com/a/957278
+### MAKE SURE TO USE BASH not DASH
+## Define array of all dirs to be created like:
+# array_dirs=( "${PATH_ENV}/parent_dir/{dir1,dir2{dir2_1, dir2_2}}" "${SECOND_PATH_ENV}"/dir /path )
+
