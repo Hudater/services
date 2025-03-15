3be8adced7d85308e0d86c918041aa755690cfa1
Harshit Mani Tripathi <hmanitops@gmail.com>
2025-03-16 01:05:13 +0530

    TODO: pihole on docker with updatelist and autharatative unbound dns

---
+++ docker/pihole/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+CREATE_DIRS=( "${BAK_CFG_DIR}"/pihole/{multiple,dir,names} "$DB_CFG_DIR"/serviceNameDb "$DB_CFG_DIR"/authentik_postgres )
+mkdir -p "${CREATE_DIRS[@]}"
+# ls -alh "$BAK_CFG_DIR"/pihole/ "$DB_CFG_DIR"
+ls -alh "${CREATE_DIRS[@]}"
+docker compose -f "${COMPOSE_DIR}/pihole/docker-compose.yml" up -d --force-recreate
+
+
+### Credit: https://askubuntu.com/a/957278
+### MAKE SURE TO USE BASH not DASH
+## Define array of all dirs to be created like:
+# array_dirs=( "${PATH_ENV}/parent_dir/{dir1,dir2{dir2_1, dir2_2}}" "${SECOND_PATH_ENV}"/dir /path )
+
