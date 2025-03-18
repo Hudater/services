764b1592d567224b6cdf6464209a10e09ab59e8e
Harshit Mani Tripathi <hmanitops@gmail.com>
2025-03-18 20:39:56 +0530

    mapped random volumes to dirs

--- docker/netbird/deploy.sh
+++ docker/netbird/deploy.sh
@@ -1,8 +1,3 @@
-### Credit: https://askubuntu.com/a/957278
-### MAKE SURE TO USE BASH not DASH
-## Define array of all dirs to be created like:
-# array_dirs=( "${PATH_ENV}/parent_dir/{dir1,dir2{dir2_1, dir2_2}}" "${SECOND_PATH_ENV}"/dir /path )
-
-
 #!/bin/bash
+mkdir -p "${CFG_DIR}"/netbird/coturn
 docker compose up -d
--- docker/openspeedtest/deploy.sh
+++ docker/openspeedtest/deploy.sh
@@ -1,2 +1,4 @@
-mkdir -p "${CFG_DIR}"/openspeedtest #change me
+#!/bin/bash
+CREATE_DIRS=( "${CFG_DIR}"/openspeedtest{config,letsencrypt} )
+mkdir -p "${CREATE_DIRS[@]}"
 docker compose up -d
