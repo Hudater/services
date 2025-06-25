8157b913bcd27921fcb7668f132b2c0efc586291
Harshit Mani Tripathi <harshit@hudater.dev>
2025-06-25 22:38:07 +0530

    gpu passthrough crashed so moving to non-gpu setup temp

---
+++ docker/rclone/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+CREATE_DIRS=( "${BAK_CFG_DIR}"/rclone/{multiple,dir,names} "$DB_CFG_DIR"/serviceName "$CFG_DIR"/serviceName )
+mkdir -p "${CREATE_DIRS[@]}"
+# ls -alh "$BAK_CFG_DIR"/rclone/ "$DB_CFG_DIR"
+ls -alh "${CREATE_DIRS[@]}"
+docker compose  --env-file "$BAK_CFG_DIR/rclone/.env" -f "${COMPOSE_DIR}/serviceName/docker-compose.yml" up -d --force-recreate
+
+
+### Credit: https://askubuntu.com/a/957278
+### MAKE SURE TO USE BASH not DASH
+## Define array of all dirs to be created like:
+# array_dirs=( "${PATH_ENV}/parent_dir/{dir1,dir2{dir2_1, dir2_2}}" "${SECOND_PATH_ENV}"/dir /path )
+
