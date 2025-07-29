aeb2bc28bbc5f1ca8355e6dd1886b8c79561a87e
Harshit Mani Tripathi <harshit@hudater.dev>
2025-07-29 15:47:08 +0530

    fix qbit to lsio with docker_mod to look at my ISOs being downloaded in beautiful ui

--- docker/qbittorrent/deploy.sh
+++ docker/qbittorrent/deploy.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
 mkdir -p "${CFG_DIR}"/qbittorrent
+mkdir -p "${DATA_DIR}"/Torrents/{complete,downloading,ISO,Movies,MusicArr,Prowlarr,Shows,torfiles/{added,complete}}
 docker compose up -d
